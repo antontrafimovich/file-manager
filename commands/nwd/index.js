75989f4870063343840441a0c4c01d7740c97c02
Anton Trafimovich <antont@compatibl.com>
2022-12-17 18:32:16 +0100

    fix: add common execute function to cd.

--- commands/nwd/index.js
+++ commands/nwd/index.js
@@ -1,30 +1,21 @@
-import { commandsEmitter } from '../../emitter.js';
-import { cd } from './cd.js';
-import { ls } from './ls.js';
-import { up } from './up.js';
-
-const execute = async (command) => {
-  try {
-    await command();
-  } catch (error) {
-    commandsEmitter.emit("error", { message: "Invalid Input" });
-  } finally {
-    commandsEmitter.emit("commandEnd");
-  }
-};
+import { commandsEmitter, execute } from "../../emitter.js";
+import { throwInvalidInputError } from "../../utils/error.js";
+import { cd } from "./cd.js";
+import { ls } from "./ls.js";
+import { up } from "./up.js";
 
 commandsEmitter
   .on("up", () => execute(() => up()))
-  .on("cd", async (params) => {
+  .on("cd", (params) => {
     execute(async () => {
       if (params.length > 1) {
-        throw new Error("Invalid input");
+        throwInvalidInputError();
       }
 
       const [changeDirectoryTo] = params;
       await cd(changeDirectoryTo);
     });
   })
-  .on("ls", async () => {
+  .on("ls", () => {
     execute(() => ls());
   });

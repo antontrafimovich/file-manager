9de1928381c0c54fb80cc4c220c77a7c5fb43cd9
Anton Trafimovich <antont@compatibl.com>
2022-12-17 18:31:42 +0100

    fix: refactor cd implementation with new functionality.

--- commands/nwd/cd.js
+++ commands/nwd/cd.js
@@ -3,22 +3,19 @@ import path from "node:path";
 
 import { currentDir } from "../../cwd.js";
 import store from "../../store.js";
+import { throwOperationFailedError } from "../../utils/error.js";
 
-export const cd = async (changeDirectoryTo) => {
-  if (!changeDirectoryTo) {
-    throw new Error("Path to new directory must be valid");
-  }
-
-  let newDir = path.resolve(currentDir, changeDirectoryTo);
+export const cd = async (changeDirectoryTo = "") => {
+  const newDir = path.resolve(currentDir, changeDirectoryTo);
 
   try {
     const entry = await lstat(newDir);
 
     if (!entry.isDirectory()) {
-      throw new Error("Invalid input");
+      throwOperationFailedError();
     }
-  } catch (error) {
-    throw new Error("Invalid input");
+  } catch {
+    throwOperationFailedError();
   }
 
   store.trigger({ type: "SET_WORKING_DIR", payload: newDir });

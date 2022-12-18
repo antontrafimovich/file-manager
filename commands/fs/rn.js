c8b2cad07041b0668efabbd1cf720915de068e4c
Anton Trafimovich <antont@compatibl.com>
2022-12-18 14:41:02 +0100

    fix: display invalid input error in case of renaming directory.

--- commands/fs/rn.js
+++ commands/fs/rn.js
@@ -13,6 +13,19 @@ export const rn = async (pathToFile, newFilename) => {
   }
 
   const originalFilePath = path.resolve(currentDir, pathToFile);
+
+  let entry;
+
+  try {
+    entry = await lstat(originalFilePath);
+  } catch {
+    throwOperationFailedError();
+  }
+
+  if (!entry.isFile()) {
+    throwInvalidInputError();
+  }
+
   const originalFileDirname = path.dirname(originalFilePath);
 
   const newFilePath = path.resolve(originalFileDirname, newFilename);
@@ -23,12 +36,6 @@ export const rn = async (pathToFile, newFilename) => {
   }
 
   try {
-    const entry = await lstat(originalFilePath);
-
-    if (!entry.isFile()) {
-      throwOperationFailedError();
-    }
-
     await rename(originalFilePath, newFilePath);
   } catch (error) {
     throwOperationFailedError();

7991d399b8336a84b648f050d79ef1f0a924baed
Anton Trafimovich <antont@compatibl.com>
2022-12-19 11:19:39 +0100

    feat: add validation to compress command.

--- commands/zip/compress.js
+++ commands/zip/compress.js
@@ -1,26 +1,53 @@
 import { createReadStream, createWriteStream } from "node:fs";
+import { lstat } from "node:fs/promises";
+import path from "node:path";
 import { pipeline } from "node:stream/promises";
 import { createBrotliCompress } from "node:zlib";
-import path from "node:path";
 
-import store from "./../../store.js";
-
-let currentDir = "";
-
-store.onUpdate((state) => {
-  currentDir = state.workingDirectory;
-});
+import {
+  throwInvalidInputError,
+  throwOperationFailedError,
+} from "../../utils/error.js";
+import { currentDir } from "./../../cwd.js";
 
 export const compress = async (pathToFile, pathToDestination) => {
+  if (!pathToFile || !pathToDestination) {
+    throwInvalidInputError();
+  }
+
   const fileToCompressPath = path.resolve(currentDir, pathToFile);
+
+  let fileToCompressStat;
+
+  try {
+    fileToCompressStat = await lstat(fileToCompressPath);
+  } catch {
+    throwOperationFailedError();
+  }
+
+  if (!fileToCompressStat.isFile()) {
+    throwInvalidInputError();
+  }
+
   const archivePath = path.resolve(currentDir, pathToDestination);
+
+  let archivePathStat;
+
+  try {
+    archivePathStat = await lstat(archivePath);
+  } catch {}
+
+  if (archivePathStat !== undefined) {
+    throwOperationFailedError();
+  }
 
   const brotli = createBrotliCompress();
   const r = createReadStream(fileToCompressPath);
   const w = createWriteStream(archivePath);
+
   try {
     await pipeline(r, brotli, w);
-  } catch (error) {
-    throw new Error("Invalid input");
+  } catch {
+    throwOperationFailedError();
   }
 };
--- commands/zip/index.js
+++ commands/zip/index.js
@@ -1,19 +1,19 @@
-import { commandsEmitter } from "../../emitter.js";
+import { commandsEmitter, execute } from "../../emitter.js";
+import { throwInvalidInputError } from "../../utils/error.js";
 import { compress } from "./compress.js";
 import { decompress } from "./decompress.js";
 
-const execute = async (command) => {
-  try {
-    await command();
-  } catch (error) {
-    commandsEmitter.emit("error", error);
-  }
-};
-
 commandsEmitter
-  .on("compress", async ([pathToFile, pathToDestination]) => {
-    execute(() => compress(pathToFile, pathToDestination));
-  })
+  .on("compress", (params) =>
+    execute(async () => {
+      if (params.length > 2) {
+        throwInvalidInputError();
+      }
+
+      const [pathToFile, pathToDestination] = params;
+      await compress(pathToFile, pathToDestination);
+    })
+  )
   .on("decompress", async ([pathToFile, pathToDestination]) => {
     execute(() => decompress(pathToFile, pathToDestination));
   });
